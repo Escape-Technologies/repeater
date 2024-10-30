1a9fb3b4995493865a1d7cc4e1013d2611ee0594
QuentinN42 <quentin@lieumont.fr>
2024-10-30 19:14:32 +0100

    feat: backend implem
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- pkg/grpc/utils.go
+++ pkg/grpc/utils.go
@@ -1,10 +1,14 @@
 package grpc
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"log"
+	"net"
+	"net/url"
 	"strings"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -13,10 +17,32 @@ import (
 
 var maxMsgSize = 1024 * 1024 * 1024
 
+// Custom dialler for gRPC connection via proxy
+func customDialer(proxyURL string) func(context.Context, string) (net.Conn, error) {
+	return func(ctx context.Context, target string) (net.Conn, error) {
+		proxy, err := url.Parse(proxyURL)
+		if err != nil {
+			return nil, err
+		}
+		// Establish HTTP2-compatible connection via proxy
+		dialer := &net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 10 * time.Second,
+		}
+		return dialer.DialContext(ctx, "tcp", proxy.Host)
+	}
+}
+
 func GetCon(url, proxyURL string) *grpc.ClientConn {
-	var creds grpc.DialOption
-	if strings.Split(url, ":")[0] == "localhost" {
-		creds = grpc.WithTransportCredentials(insecure.NewCredentials())
+	opts := []grpc.DialOption{
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(maxMsgSize),
+			grpc.MaxCallSendMsgSize(maxMsgSize),
+		),
+	}
+
+	if strings.HasPrefix(url, "localhost") {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	} else {
 		systemRoots, err := x509.SystemCertPool()
 		if err != nil {
@@ -25,16 +51,14 @@ func GetCon(url, proxyURL string) *grpc.ClientConn {
 		cred := credentials.NewTLS(&tls.Config{
 			RootCAs: systemRoots,
 		})
-		creds = grpc.WithTransportCredentials(cred)
+		opts = append(opts, grpc.WithTransportCredentials(cred))
 	}
-	con, err := grpc.NewClient(
-		url,
-		creds,
-		grpc.WithDefaultCallOptions(
-			grpc.MaxCallRecvMsgSize(maxMsgSize),
-			grpc.MaxCallSendMsgSize(maxMsgSize),
-		),
-	)
+
+	if proxyURL != "" {
+		opts = append(opts, grpc.WithContextDialer(customDialer(proxyURL)))
+	}
+
+	con, err := grpc.NewClient(url, opts...)
 	if err != nil {
 		log.Fatalf("Error connecting: %v \n", err)
 	}
