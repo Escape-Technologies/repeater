b3e83bf34d214b46bf911abba5d638b9645a59b1
QuentinN42 <quentin@lieumont.fr>
2024-10-31 12:07:15 +0100

    fix: entire proxy refacto
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- pkg/grpc/utils.go
+++ pkg/grpc/utils.go
@@ -1,37 +1,28 @@
 package grpc
 
 import (
+	"bufio"
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"net/url"
 	"strings"
+	"syscall"
 	"time"
 
+	"golang.org/x/sys/unix"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 var maxMsgSize = 1024 * 1024 * 1024
-
-// Custom dialler for gRPC connection via proxy
-func customDialer(proxyURL string) func(context.Context, string) (net.Conn, error) {
-	return func(ctx context.Context, target string) (net.Conn, error) {
-		proxy, err := url.Parse(proxyURL)
-		if err != nil {
-			return nil, err
-		}
-		// Establish HTTP2-compatible connection via proxy
-		dialer := &net.Dialer{
-			Timeout:   10 * time.Second,
-			KeepAlive: 10 * time.Second,
-		}
-		return dialer.DialContext(ctx, "tcp", proxy.Host)
-	}
-}
 
 func GetCon(url, proxyURL string) *grpc.ClientConn {
 	opts := []grpc.DialOption{
@@ -55,7 +46,7 @@ func GetCon(url, proxyURL string) *grpc.ClientConn {
 	}
 
 	if proxyURL != "" {
-		opts = append(opts, grpc.WithContextDialer(customDialer(proxyURL)))
+		opts = append(opts, grpc.WithContextDialer(proxyDialer(proxyURL)))
 	}
 
 	con, err := grpc.NewClient(url, opts...)
@@ -63,4 +54,97 @@ func GetCon(url, proxyURL string) *grpc.ClientConn {
 		log.Fatalf("Error connecting: %v \n", err)
 	}
 	return con
+}
+
+type bufConn struct {
+	net.Conn
+	r io.Reader
+}
+
+func (c *bufConn) Read(b []byte) (int, error) {
+	return c.r.Read(b)
+}
+
+func netDialerWithTCPKeepalive() *net.Dialer {
+	return &net.Dialer{
+		// Setting a negative value here prevents the Go stdlib from overriding
+		// the values of TCP keepalive time and interval. It also prevents the
+		// Go stdlib from enabling TCP keepalives by default.
+		KeepAlive: time.Duration(-1),
+		// This method is called after the underlying network socket is created,
+		// but before dialing the socket (or calling its connect() method). The
+		// combination of unconditionally enabling TCP keepalives here, and
+		// disabling the overriding of TCP keepalive parameters by setting the
+		// KeepAlive field to a negative value above, results in OS defaults for
+		// the TCP keealive interval and time parameters.
+		Control: func(_, _ string, c syscall.RawConn) error {
+			return c.Control(func(fd uintptr) {
+				unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_KEEPALIVE, 1)
+			})
+		},
+	}
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func sendHTTPRequest(ctx context.Context, req *http.Request, conn net.Conn) error {
+	req = req.WithContext(ctx)
+	if err := req.Write(conn); err != nil {
+		return fmt.Errorf("failed to write the HTTP request: %v", err)
+	}
+	return nil
+}
+
+func doHTTPConnectHandshake(ctx context.Context, conn net.Conn, backendAddr string, proxyURL url.URL) (_ net.Conn, err error) {
+	defer func() {
+		if err != nil {
+			conn.Close()
+		}
+	}()
+
+	req := &http.Request{
+		Method: http.MethodConnect,
+		URL:    &url.URL{Host: backendAddr},
+		Header: make(http.Header),
+	}
+	if t := proxyURL.User; t != nil {
+		u := t.Username()
+		p, _ := t.Password()
+		req.Header.Add("Proxy-Authorization", "Basic "+basicAuth(u, p))
+	}
+
+	if err := sendHTTPRequest(ctx, req, conn); err != nil {
+		return nil, fmt.Errorf("failed to write the HTTP request: %v", err)
+	}
+
+	r := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(r, req)
+	if err != nil {
+		return nil, fmt.Errorf("reading server HTTP response: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to do connect handshake, status code: %s", resp.Status)
+	}
+
+	return &bufConn{Conn: conn, r: r}, nil
+}
+
+func proxyDialer(proxy string) func(context.Context, string) (net.Conn, error) {
+	return func(ctx context.Context, addr string) (net.Conn, error) {
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			return nil, err
+		}
+		proxyAddr := proxyURL.Host
+
+		conn, err := netDialerWithTCPKeepalive().DialContext(ctx, "tcp", proxyAddr)
+		if err != nil {
+			return nil, err
+		}
+		return doHTTPConnectHandshake(ctx, conn, addr, *proxyURL)
+	}
 }
